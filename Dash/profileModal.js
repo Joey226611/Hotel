26d057afac19f4ae019510547ed08b420d6a01bf
Joey226611 <157316686+Joey226611@users.noreply.github.com>
2026-04-20 15:49:37 +0200

    Delete Dash/profileModal.js

--- Dash/profileModal.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-
-  const modal = document.getElementById("profileModal");
-
-  window.closeProfile = () => {
-    modal?.classList.add("hidden");
-  };
-
-});
