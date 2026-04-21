d73ee50b2b258e56403cbbb40809e6ae9f28e100
Joey226611 <157316686+Joey226611@users.noreply.github.com>
2026-04-21 17:55:49 +0200

    Add Firebase initialization in firebase.js

---
+++ Dash/firebase.js
@@ -0,0 +1,18 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
+
+ const firebaseConfig = {
+  apiKey: "AIzaSyBeidcBbNTGKb_GtXEad20Xpug1Se9e9x0",
+  authDomain: "hotelreception.firebaseapp.com",
+ databaseURL: "https://hotelreception-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "hotelreception",
+  storageBucket: "hotelreception.firebasestorage.app",
+  messagingSenderId: "162982201655",
+  appId: "1:162982201655:web:e7730850e09d3670d032b2"
+};
+
+
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+
+console.log("🔥 Dashboard Firebase ready");
