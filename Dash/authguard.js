5361ca98e2ee2f0749c3ae241519d8d947ef2d8a
Joey226611 <157316686+Joey226611@users.noreply.github.com>
2026-04-22 10:24:25 +0200

    Update and rename authGuard.js to authguard.js

--- Dash/authguard.js
+++ Dash/authguard.js
@@ -1,16 +1,22 @@
 import { auth } from "./firebase.js";
 import { onAuthStateChanged } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
 
-// wacht tot auth geladen is
+const ADMIN_EMAIL = "jey3602@gmail.com";
+
 onAuthStateChanged(auth, (user) => {
 
   // ❌ Niet ingelogd → terug naar login
   if (!user) {
-    console.log("⛔ Not logged in → redirect");
     window.location.href = "../Start/Login.html";
     return;
   }
 
-  // ✅ Wel ingelogd
-  console.log("✅ User authenticated:", user.email);
+  // 👤 Zet email global zodat dashboard.js hem kan gebruiken
+  window.currentUser = user;
+
+  // ⭐ Admin check
+  window.isAdmin = user.email === ADMIN_EMAIL;
+
+  // Laat pagina pas zien wanneer auth klaar is
+  document.body.style.visibility = "visible";
 });
